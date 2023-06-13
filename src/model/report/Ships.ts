fdb588b90b4dca14c2429a460e31ca0246f1cc12
FQS <oliver@riesewohld.net>
2023-06-13 13:47:08 +0200

    added modifiers to json models

--- src/model/report/Ships.ts
+++ src/model/report/Ships.ts
@@ -1,12 +1,15 @@
 import { JsonProperty } from "json-object-mapper";
 
 import { ShipList } from "./ShipList";
+import { Modifier } from "./Modifier";
 
 export class Ships {
   @JsonProperty({ name: "result", type: ShipList })
   private _survivingShips?: ShipList;
   @JsonProperty({ name: "g", type: ShipList })
   private _fightingShips?: ShipList;
+  @JsonProperty({ name: "mod", type: Modifier })
+  private _modifiers?: Modifier;
 
   get survivingShips(): ShipList {
     if (this._survivingShips == undefined) {
@@ -20,5 +23,11 @@ export class Ships {
       throw new Error("fightingShips property is required");
     }
     return this._fightingShips;
+  }
+
+  get modifiers(): Modifier {
+    if (this._modifiers == undefined)
+      throw new Error("modifier property is required");
+    return this._modifiers;
   }
 }
